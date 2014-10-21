41d62116050c507a4420a3defc9047b7ee6c807f
Paul Anderson <panderson@add123.com>
2014-10-21 17:12:10 -0400

    removing old slimerjs stuff

--- server.js
+++ server.js
@@ -1,41 +1,10 @@
 var path = require('path'),
-	childProcess = require('child_process'),
-	slimerjs = require('slimerjs'),
 	express = require('express'),
 	bodyParser = require('body-parser'),
 	app = express(),
-	binPath = slimerjs.path,
-	slimerPath  = path.join(__dirname, 'slimer.js'),
 	request = require('request');
 
 var fs = require('fs');
-
-
-function execSlimer(url, callback) {
-	var filename = 'pics/' + new Date().getTime() + Math.round(Math.random()*10000) + '.png';
-	var childArgs = [
-		path.join(__dirname, 'slimer.js'),
-		url,
-		path.join(__dirname, 'public/' + filename)
-	];
-
-	childProcess.execFile(binPath, childArgs, function(err, stdout, stderr) {
-		console.log(stdout);
-		callback(filename);
-	});
-}
-
-function getPic(req, res) {
-
-	console.log(req.body);
-	if(!req.body.url) {
-		res.end();
-	}
-	execSlimer(req.body.url, function(filename) {
-		var resBody = {'filename':filename};
-		res.json(resBody);
-	});
-}
 
 function incrementViewCount(req, res, next) {
 	fs.readFile('viewCount', function(err, data) {
