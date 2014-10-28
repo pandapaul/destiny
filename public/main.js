3c68f5e010f558ab83fde0da8acbe1f14f733f77
Paul Anderson <thesamuraipanda@gmail.com>
2014-10-28 17:25:16 -0400

    adding weekly heroic & nightfall tracking

--- public/main.js
+++ public/main.js
@@ -33,6 +33,54 @@ $(function() {
 			weeklyMarks: {
 				2033897742: 'Weekly Vanguard Marks',
 				2033897755: 'Weekly Crucible Marks'
+			},
+			weeklyHeroics: {
+				2591274210: {name: "The Devil's Lair", level: 22},
+				1178608439: {name: "The Summoning Pits", level: 22},
+				1100096652: {name: "The Nexus", level: 22},
+				3304208793: {name: "Cerberus Vae III", level: 22},
+				2495639390: {name: "Winter's Run", level: 22},
+				906746307: {name: "The Devil's Lair", level: 22},
+				1748464680: {name: "The Summoning Pits", level: 22},
+				3815863573: {name: "The Nexus", level: 22},
+				3037962954: {name: "Cerberus Vae III", level: 22},
+				670913375: {name: "Winter's Run", level: 22},
+				2591274211: {name: "The Devil's Lair", level: 26},
+				1178608438: {name: "The Summoning Pits", level: 26},
+				1100096653: {name: "The Nexus", level: 26},
+				3304208792: {name: "Cerberus Vae III", level: 26},
+				2495639391: {name: "Winter's Run", level: 26},
+				906746306: {name: "The Devil's Lair", level: 26},
+				1748464681: {name: "The Summoning Pits", level: 26},
+				3815863572: {name: "The Nexus", level: 26},
+				3037962955: {name: "Cerberus Vae III", level: 26},
+				670913374: {name: "Winter's Run", level: 26},
+				2591274208: {name: "The Devil's Lair", level: 28},
+				1178608437: {name: "The Summoning Pits", level: 28},
+				1100096654: {name: "The Nexus", level: 28},
+				3304208795: {name: "Cerberus Vae III", level: 28},
+				2495639388: {name: "Winter's Run", level: 28},
+				906746305: {name: "The Devil's Lair", level: 28},
+				1748464682: {name: "The Summoning Pits", level: 28},
+				3815863575: {name: "The Nexus", level: 28},
+				3037962952: {name: "Cerberus Vae III", level: 28},
+				670913373: {name: "Winter's Run", level: 28}
+			},
+			weeklyNightfalls: {
+				2591274209: "The Devil's Lair",
+				1178608436: "The Summoning Pits",
+				1100096655: "The Nexus",
+				3304208794: "Cerberus Vae III",
+				2495639389: "Winter's Run",
+				906746304: "The Devil's Lair",
+				1748464683: "The Summoning Pits",
+				3815863574: "The Nexus",
+				3037962953: "Cerberus Vae III",
+				670913372: "Winter's Run"
+			},
+			vaultOfGlass: {
+				2659248071: {name: "Vault of Glass", level: 26},
+				2659248068: {name: "Vault of Glass", level: 30}
 			}
 		},
 		mostRecentCharacterDate = null;
@@ -161,7 +209,23 @@ $(function() {
 		return dfd;
 	}
 
+	function getDateOfMostRecentReset() {
+		var date = new Date();
+		var currentDayOfWeek = date.getUTCDay();
+		var distanceToMostRecentTuesday = currentDayOfWeek - 2;
+		if(distanceToMostRecentTuesday < 0 || (distanceToMostRecentTuesday === 0 && date.getUTCHours() < 9)) {
+			distanceToMostRecentTuesday += 7;
+		}
+		date.setUTCDate(date.getUTCDate() - distanceToMostRecentTuesday);
+		date.setUTCHours(9);
+		date.setUTCMinutes(0);
+		date.setUTCSeconds(0);
+		date.setUTCMilliseconds(0);
+		return date;
+	}
+
 	function loadCharacterInfo(character, isLastCharacter) {
+		var mostRecentResetDate = getDateOfMostRecentReset();
 		var profileHref = 'http://www.bungie.net/en/Legend/' + character.characterBase.membershipType + '/' + character.characterBase.membershipId + '/' + character.characterBase.characterId;
 		getCurrency(character.characterBase)
 		.done(function (res) {
@@ -172,19 +236,34 @@ $(function() {
 					d.append(',');
 				}
 			}
-			var w = $('<div/>')
-				.addClass('character-weekly-marks')
-				.appendTo(d);
+			var a = $('<div/>')
+					.addClass('character-activities')
+					.appendTo(d),
+				w = $('<div/>')
+					.addClass('character-weekly-marks')
+					.appendTo(d),
+				characterDate = new Date(character.characterBase.dateLastPlayed),
+				playedSinceReset = characterDate - mostRecentResetDate > 0;
+			if(playedSinceReset) {
+				getActivities(character.characterBase)
+				.done(function (res) {
+					showActivityCompletion(a, res);
+				});
+			} else {
+				showActivityCompletion(a);
+			}
 			getProgress(character.characterBase)
 			.done(function (res) {
 				for(var i=0;i<res.length;i++) {
 					if(hashes[res[i].progressionHash]) {
 						d.append(buildProgressBar(res[i]));
 					} else if(hashes.weeklyMarks[res[i].progressionHash]) {
+						if(!playedSinceReset) {
+							res[i].level = 0;
+						}
 						w.append(buildMarksBar(res[i]));
 					}
 				}
-				var characterDate = new Date(character.characterBase.dateLastPlayed);
 				if(!mostRecentCharacterDate || characterDate - mostRecentCharacterDate > 0) {
 					d.prependTo(characters);
 					mostRecentCharacterDate = characterDate;
@@ -293,6 +372,78 @@ $(function() {
 			}
 		);
 		return dfd;
+	}
+
+	function getActivities(characterBase) {
+		var dfd = new $.Deferred(),
+			accountType = 'TigerPSN';
+
+		if(characterBase.membershipType === 1) {
+			accountType = 'TigerXbox';
+		}
+
+		jsonp('http://www.bungie.net/Platform/Destiny/' + accountType + '/Account/' + characterBase.membershipId + '/Character/' + characterBase.characterId + '/Activities/',
+			function(data) {
+				if(data && data.Response && data.Response.data && data.Response.data.available && data.Response.data.available.length) {
+					return dfd.resolve(data.Response.data.available);
+				} else {
+					dfd.reject(errNoResponseFromBungie);
+				}
+			},
+			function(err) {
+				dfd.reject(err);
+			}
+		);
+		return dfd;
+	}
+
+	function showActivityCompletion(div, data) {
+		var activity = null,
+			heroic = null,
+			nightfall = null,
+			vog = null;
+		if(data) {
+			for(i=0;i<data.length;i++) {
+				if(data[i].isCompleted) {
+					activity = hashes.weeklyHeroics[data[i].activityHash];
+					if((activity && !heroic) || (activity && heroic && activity.level > heroic.level)) {
+						heroic = activity;
+						continue;
+					}
+					activity = hashes.weeklyNightfalls[data[i].activityHash];
+					if(activity) {
+						nightfall = activity;
+						continue;
+					}
+					activity = hashes.vaultOfGlass[data[i].activityHash];
+					if((activity && !vog) || (activity && vog && activity.level > vog.level)) {
+						vog = activity;
+						continue;
+					}
+				}
+			}
+		}
+		var heroicCompletionText = 'Weekly Heroic ';
+		if(heroic) {
+			heroicCompletionText += '(' + heroic.level + ') Complete';
+		} else {
+			heroicCompletionText += 'Incomplete';
+		}
+		var nightfallCompletionText = 'Weekly Nightfall ';
+		if(nightfall) {
+			nightfallCompletionText += 'Complete';
+		} else {
+			nightfallCompletionText += 'Incomplete';
+		}
+		var vogCompletionText = 'Vault of Glass ';
+		if(vog) {
+			vogCompletionText += '(' + vog.level + ') Complete';
+		} else {
+			vogCompletionText += 'Incomplete';
+		}
+		div.append($('<span/>').text(heroicCompletionText + ', '))
+			.append($('<span/>').text(nightfallCompletionText))
+			// .append($('<span/>').text(vogCompletionText)); TODO gotta figure out how raid progress/completion works
 	}
 
 	function buildProgressBar(progressionData) {
