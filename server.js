6fd62147d80ff187cadd9f0da4e583251e586c72
Paul Anderson <thesamuraipanda@gmail.com>
2014-11-28 10:53:08 -0500

    building a super early preview leaderboard thing

--- server.js
+++ server.js
@@ -366,7 +366,7 @@ function DatabaseConnectionHandler() {
 
 function leaderboard(req, res) {
 	var currentProgressField = {};
-	var fetcher = new Fetcher({},{sort:{'progressions.529303302.currentProgress':-1}, fields:{'_id':0, 'membership':1, 'id':1, 'progressions.529303302.currentProgress':1}});
+	var fetcher = new Fetcher({},{sort:{'progressions.529303302.currentProgress':-1}, fields:{'_id':0, 'membership':1, 'id':1, 'progressions.529303302.currentProgress':1, 'progressions.529303302.level':1}});
 	// var fetcher = new Fetcher();
 	fetcher.fetch();
 	fetcher.finished(function(docs) {
